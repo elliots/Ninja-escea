0b28b4468b20fb913f4cbdf1bb82da76ca15e1f4
Mark Wolfe <mark@wolfe.id.au>
2014-05-05 21:42:49 +1000

    Tweaked the UDP bind and a bit of a tidy.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var escea_udp = require('./lib/escea_udp')
-  , escea_switch = require ('./lib/escea_switch')
+  , escea_switch = require('./lib/escea_switch')
   , escea_room = require('./lib/escea_room')
   , escea_target = require('./lib/escea_target')
   , util = require('util')
   , stream = require('stream')
-  , events = require('events'); 
-  
+  , events = require('events');
+
 
 // Give our driver a stream interface
-util.inherits(escea,stream);
+util.inherits(escea, stream);
 
 // Our greeting to the user.
 
@@ -27,36 +27,36 @@ util.inherits(escea,stream);
  * @fires register - Emit this when you wish to register a device (see Device)
  * @fires config - Emit this when you wish to send config data back to the Ninja Platform
  */
-function escea(opts,app) {
+function escea(opts, app) {
 
   var self = this;
   this.first = true;
   this._opts = opts;
-  
+
   console.log("start");
 
-  app.on('client::up',function(){
+  app.on('client::up', function() {
 
     // The client is now connected to the Ninja Platform
 
     // Check if we have sent an announcement before.
     // If not, send one and save the fact that we have.
-    var escea_comms = new escea_udp;
-    
-     processFires = function(serial) {
-             console.log("Serial = " + serial);
-             var eventemitter = new events.EventEmitter();
-             self.emit('register', new escea_switch(escea_comms,eventemitter,serial));
-             self.emit('register', new escea_room(serial,eventemitter));
-             self.emit('register', new escea_target(serial,eventemitter));
-      }
-   
-     if (self.first) {
-               escea_comms.discover(processFires);
-               self.first = false;
-     }          
+    var escea_comms = new Escea_udp;
+
+    processFires = function(serial) {
+      console.log("Serial = " + serial);
+      var eventemitter = new events.EventEmitter();
+      self.emit('register', new escea_switch(escea_comms, eventemitter, serial));
+      self.emit('register', new escea_room(serial, eventemitter));
+      self.emit('register', new escea_target(serial, eventemitter));
+    };
+
+    if (self.first) {
+      escea_comms.discover(processFires);
+      self.first = false;
+    }
   });
-};
+}
 
 // Export it
 module.exports = escea;
